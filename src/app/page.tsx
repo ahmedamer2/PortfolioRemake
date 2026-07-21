df74ab7dd42e5909c6ed555c0355da4d2ac0ab79
Ahmed Amer <ahmedsay01@gmail.com>
2026-07-21 11:58:16 -0600

    Added sanity and Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hello World
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
