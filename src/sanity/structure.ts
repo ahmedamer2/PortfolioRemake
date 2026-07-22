49bcb4978d41d128e38d08e43831076760737239
Ahmed Amer <ahmedsay01@gmail.com>
2026-07-22 15:22:04 -0600

    Schema complete

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,5 +1,15 @@
+import { UserIcon } from "@sanity/icons";
 import type { StructureResolver } from "sanity/structure";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
-  S.list().title("Content").items(S.documentTypeListItems());
+  S.list()
+    .title("Portfolio Content")
+    .items([
+      S.listItem()
+        .title("Profile")
+        .icon(UserIcon)
+        .child(
+          S.document().schemaType("profile").documentId("singleton-profile"),
+        ),
+    ]);
