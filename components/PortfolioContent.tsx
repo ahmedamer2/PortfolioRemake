53706607e743d9e1c25fc7bf0d4719be04e18a28
Ahmed Amer <63519817+ahmedamer2@users.noreply.github.com>
2026-07-23 20:41:01 -0600

    [Bug] Fix Linting Issues (#22)

--- components/PortfolioContent.tsx
+++ components/PortfolioContent.tsx
@@ -1,11 +1,7 @@
 import HeroSection from "@/components/sections/HeroSection";
 
 async function PortfolioContent() {
-  return (
-    <>
-      <HeroSection />
-    </>
-  );
+  return <HeroSection />;
 }
 
 export default PortfolioContent;
